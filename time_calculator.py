9c7900672ae268cdc361957c53525ba41b295fea
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-23 16:27:44 +0800

    Add variables and list of days of the week

--- time_calculator.py
+++ time_calculator.py
@@ -1,4 +1,20 @@
-def add_time(start, duration):
+def add_time(start, duration, *day):
+    new_time = ''
+    new_hour = ''
+    new_minute = ''
+    new_meridiem = ''
+    new_day = ''
+    day_of_week = ''
+    day_statement = ''
+    days_of_week = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
 
 
 
